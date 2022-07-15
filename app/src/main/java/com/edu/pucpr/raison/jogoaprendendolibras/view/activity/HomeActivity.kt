3d95fad24b7392b57bc3c05b40538896882532bc
Raison Robert Monteiro de Sá <raison.robert@pucpr.edu.br>
2022-07-14 21:05:11 -0400

    Tela de Criar Conta
    Implementado tela de Criar Conta

---
+++ app/src/main/java/com/edu/pucpr/raison/jogoaprendendolibras/view/activity/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.edu.pucpr.raison.jogoaprendendolibras.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.edu.pucpr.raison.jogoaprendendolibras.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(
+            R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/edu/pucpr/raison/jogoaprendendolibras/view/fragment/LoginFragment.kt
+++ app/src/main/java/com/edu/pucpr/raison/jogoaprendendolibras/view/fragment/LoginFragment.kt
@@ -1,11 +1,16 @@
 package com.edu.pucpr.raison.jogoaprendendolibras.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.edu.pucpr.raison.jogoaprendendolibras.R
+import com.edu.pucpr.raison.jogoaprendendolibras.view.activity.BemVindoActivity
+import com.edu.pucpr.raison.jogoaprendendolibras.view.activity.HomeActivity
+import kotlinx.android.synthetic.main.layout_fragment_login.view.*
 
 class LoginFragment : Fragment(){
     override fun onCreateView(
@@ -14,5 +19,20 @@ class LoginFragment : Fragment(){
     ): View? {
 //        loading = Load.createLoadDialog(requireContext(), false)
         return inflater.inflate(R.layout.layout_fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        botoes(view)
+    }
+
+    private fun botoes(view: View) {
+        view.criarConta.setOnClickListener {
+            findNavController().navigate(R.id.action_login_to_criar_conta)
+        }
+        view.btnLogar.setOnClickListener {
+            startActivity(Intent(requireContext(), HomeActivity::class.java))
+            requireActivity().finish()
+        }
     }
 }
